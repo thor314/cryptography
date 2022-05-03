a4521080239202b3614e9b3ea6aa22cae0fdd011
Thor <thorck@protonmail.com>
2022-05-03 14:38:56 -0700

    keygen

---
+++ d20220503_pallier/src/python/crypto_utils.py
@@ -0,0 +1,21 @@
+# cryptography utilities
+from Crypto.Util import number
+# import random
+import os
+
+
+def generate_n_p_q(n_bits):
+    p = q = n = None  # constant time allocation
+    n_len = 0
+    while n_len != n_bits:
+        # sample p,q until n is the right size
+        p, q = get_prime_over(n_bits), get_prime_over(n_bits)
+        assert(p != q)
+        n = p*q
+        n_len = n.bit_length()
+    return n, p, q
+
+
+def get_prime_over(n_bits):
+    """return a random N_BITS prime number, using pycrypto"""
+    return number.getPrime(n_bits, os.urandom)
--- d20220503_pallier/src/python/main.py
+++ d20220503_pallier/src/python/main.py
@@ -1,5 +1,10 @@
 # Make use of this library as reference:
 # https://github.com/data61/python-paillier
+# But don't worry about the features:
+# - cli
+# - float encryption (assume ints less than some number)
+# - a bunch of convenience methods
+# - tests (lol, who needs em), just write a driver, that's safe
 
 # we want 3 algorithms for encryption:
 # - setup() -> (public_info, secret_info)
@@ -8,30 +13,77 @@
 # and an addition operation over ciphertexts.
 
 # default RSA key bit length, gives more than 128 bits of security.
+from Crypto.Util import number
+from crypto_utils import *
+import random
+import os
 DEFAULT_KEYSIZE = 3072
 
 
 def main():
-    pass
+    publickey, privatekey = generate_keypair()
+
+    message = Plaintext(2357)
+    ciphertext = publickey.encrypt(message)
+    doubled_ciphertext = ciphertext.add(ciphertext)
+    doubled_message = privatekey.decrypt(doubled_ciphertext)
+
+    if doubled_message == 2357*2:
+        print("SUCCESS")
+    else:
+        print("FAIL")
 
 
-def generate_keypair():
+def generate_keypair(n_bits=DEFAULT_KEYSIZE):
     """Generate a pair of classes: PublicKey and PrivateKey"""
-    pass
+
+    n, p, q = generate_n_p_q(n_bits)
+
+    public_key = PublicKey(n)
+    private_key = PrivateKey(public_key, p, q)
+
+    return public_key, private_key
+
+
+def generate_n_of_length(n_bits):
+    """generate an RSA modulus of N_BITS"""
 
 
 class PublicKey():
     """A public key and associated encryption methods"""
+
+    def __init__(self):
+        pass
+
+    def encrypt(message):
+        """output a ciphertext using the held public key info"""
+        pass
+
     pass
 
 
 class PrivateKey():
     """A private key and associated decryption methods"""
+
+    def __init__(self):
+        pass
+
+    def decrypt(ciphertext):
     pass
+
+
+class Plaintext():
+    """Some integer message, pass input validation over it"""
+
+    def __init__(self, message):
+        self.message = message
 
 
 class Ciphertext():
     """The Pallier encryption of some plaintext"""
+
+    def __init__():
+        pass
 
 
 if __name__ == "__main__":
